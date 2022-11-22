bda57a95f8b06e83f9c3a6d0d38b4ea62431ef54
Victor Nawothnig <dminuoso+git@icloud.com>
2022-11-22 15:28:01 +0100

    Write parser polymorphic over ZeroBitType instead

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,61 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE BlockArguments         #-}
+{-# LANGUAGE CPP                    #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE MagicHash              #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE PatternSynonyms        #-}
+{-# LANGUAGE RankNTypes             #-}
+{-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE MagicHash #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE UnboxedTuples #-}
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnboxedTuples          #-}
 
 module Main where
 
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Unsafe as BS
 import qualified Data.ByteString.Internal as BS
+import qualified Data.ByteString.Unsafe as BS
 
-import Data.STRef
-import GHC.Word
-import Control.Monad.IO.Class (MonadIO(..))
-import Unsafe.Coerce (unsafeCoerce)
-import GHC.IO hiding (liftIO)
-import GHC.ST hiding (liftST)
-import GHC.Exts
-import GHC.ForeignPtr
+import           Control.Monad.IO.Class (MonadIO(..))
+import           Data.STRef
+import           GHC.Exts
+import           GHC.ForeignPtr
+import           GHC.IO hiding (liftIO)
+import           GHC.ST hiding (liftST)
+import           GHC.Word
+import           Unsafe.Coerce (unsafeCoerce)
 
+{-
 data Mode s = PureMode | IOMode | STMode s
 type family State m = r | r -> m where
   State PureMode   = State# ()
   State IOMode     = State# RealWorld
   State (STMode s) = Proxy# s
 
+ - -}
 
-newtype ParserT st e a = ParserT
+#if !MIN_VERSION_base(4,17,0)
+type ZeroBitRep = 'TupleRep ('[] :: [RuntimeRep])
+type ZeroBitType = TYPE ZeroBitRep
+#endif
+
+type IOMode = State# RealWorld
+type PureMode = Proxy# ()
+type STMode s = State# s
+
+newtype ParserT (st :: ZeroBitType) e a = ParserT
   { runParserT# :: ForeignPtrContents
                  -> Addr#
                  -> Addr#
-                 -> State st
+                 -> st
                  -> Res# st e a
   }
 
-type Res# st e a =
+type Res# (st :: ZeroBitType) e a =
   (#
-    (# State st, a, Addr# #)
-  | (# State st #)
-  | (# State st, e #)
+    (# st, a, Addr# #)
+  | (# st #)
+  | (# st, e #)
   #)
 
 data Result e a =
@@ -53,15 +64,15 @@ data Result e a =
   | Err !e                -- ^ Unrecoverble-by-default error.
   deriving Show
 
-pattern OK# :: State st -> a -> Addr# -> Res# st e a
+pattern OK# :: (st :: ZeroBitType) -> a -> Addr# -> Res# st e a
 pattern OK# st a s = (# (# st, a, s #) | | #)
 
 -- | Constructor for errors which are by default non-recoverable.
-pattern Err# :: State st -> e -> Res# st e a
+pattern Err# :: (st :: ZeroBitType) -> e -> Res# st e a
 pattern Err# st e = (# | | (# st, e #) #)
 
 -- | Constructor for recoverable failure.
-pattern Fail# :: State st -> Res# st e a
+pattern Fail# :: (st :: ZeroBitType) -> Res# st e a
 pattern Fail# st = (# | (# st #) | #)
 {-# complete OK#, Err#, Fail# #-}
 
@@ -73,7 +84,7 @@ instance Functor (ParserT s e) where
 
   (<$) a' (ParserT g) = ParserT \fp eob s st -> case g fp eob s st of
     OK# st' a s -> OK# st' a' s
-    x          -> unsafeCoerce# x
+    x           -> unsafeCoerce# x
   {-# inline (<$) #-}
 
 instance Applicative (ParserT s e) where
@@ -88,12 +99,12 @@ instance Applicative (ParserT s e) where
   ParserT fa <* ParserT fb = ParserT \fp eob s st -> case fa fp eob s st of
     OK# st' a s -> case fb fp eob s st' of
       OK# st'' b s -> OK# st'' a s
-      x -> unsafeCoerce# x
+      x            -> unsafeCoerce# x
     x -> unsafeCoerce# x
   {-# inline (<*) #-}
   ParserT fa *> ParserT fb = ParserT \fp eob s st -> case fa fp eob s st of
     OK# st' a s -> fb fp eob s st'
-    x       -> unsafeCoerce# x
+    x           -> unsafeCoerce# x
   {-# inline (*>) #-}
 
 instance Monad (ParserT s e) where
@@ -101,16 +112,17 @@ instance Monad (ParserT s e) where
   {-# inline return #-}
   ParserT fa >>= f = ParserT \fp eob s st -> case fa fp eob s st of
     OK# st' a s -> runParserT# (f a) fp eob s st'
-    x       -> unsafeCoerce# x
+    x           -> unsafeCoerce# x
   {-# inline (>>=) #-}
   (>>) = (*>)
   {-# inline (>>) #-}
 
 instance MonadIO (ParserT IOMode e) where
   liftIO (IO a) = ParserT \fp eob s rw ->
-    case a rw of 
+    case a rw of
       (# rw', a #) -> OK# rw' a s
 
+data PureWorld
 type Parser = ParserT PureMode
 type ParserIO = ParserT IOMode
 type ParserST s = ParserT (STMode s)
@@ -119,7 +131,7 @@ runParser :: Parser e a -> BS.ByteString -> Result e a
 runParser pst buf = unsafePerformIO (runParserIO (unsafeCoerce pst) buf)
 
 runParserST :: (forall s. ParserST s e a) -> BS.ByteString -> Result e a
-runParserST pst buf = unsafeDupablePerformIO (runParserIO (unsafeCoerce pst) buf)
+runParserST pst buf = unsafeDupablePerformIO (runParserIO pst buf)
 
 runParserIO :: ParserIO e a -> BS.ByteString -> IO (Result e a)
 runParserIO (ParserT f) b@(BS.PS (ForeignPtr _ fp) _ (I# len)) = do
@@ -128,7 +140,7 @@ runParserIO (ParserT f) b@(BS.PS (ForeignPtr _ fp) _ (I# len)) = do
     IO \st -> case f fp end buf st of
       OK# rw' a s ->  let offset = minusAddr# s buf
                       in (# rw', OK a (BS.drop (I# offset) b) #)
-      
+
       Err# rw' e ->  (# rw', Err e #)
       Fail# rw'  ->  (# rw', Fail #)
 
@@ -146,7 +158,7 @@ unsafeDupableEmbedIOinPure = unsafeCoerce
 
 liftST :: ST s a -> ParserST s e a
 liftST (ST t) = ParserT \fp eob s st ->
-  case unsafeCoerce t st of
+  case t st of
     (# st', a #) -> OK# st' a s
 
 thing :: ParserIO () ()
